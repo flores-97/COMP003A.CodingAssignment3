25b65ef93a0373011bde3e88302a710e76c7563e
flores-97 <floresvictor15@gmail.com>
2025-01-29 09:26:51 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ Author: Victor Flores
+Course: COMP-003A
+Faculty: Jonathon Cruz
+Purpose: Budget management application demonstrating control flow
+*/
+using System.Linq.Expressions;
+
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Budget Management Tool!\n");
+
+            int range;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter your monthly income: ");
+                    range = int.Parse(Console.ReadLine());
+                    if (range <= 0) throw new Exception("Number must be at least 1.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid Input: {ex.Message} \nPlease enter a valid income");
+                    //user's income 
+                }
+            }
+            Console.WriteLine("Menu:\n");
+            //menu line
+            Console.Write("1. Add an Expense:");
+
+            
+        }
+    }
+}
