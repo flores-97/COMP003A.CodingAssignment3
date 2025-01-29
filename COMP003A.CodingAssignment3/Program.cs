6dd39839b27b70d5d0c477bc71c09fe42ee2ae03
flores-97 <floresvictor15@gmail.com>
2025-01-29 13:00:04 -0800

    created menu and options for each. need to enter data to each section

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -4,6 +4,8 @@ Course: COMP-003A
 Faculty: Jonathon Cruz
 Purpose: Budget management application demonstrating control flow
 */
+using System.ComponentModel.Design;
+using System.Globalization;
 using System.Linq.Expressions;
 
 namespace COMP003A.CodingAssignment3
@@ -31,11 +33,54 @@ namespace COMP003A.CodingAssignment3
                     //user's income 
                 }
             }
-            Console.WriteLine("Menu:\n");
-            //menu line
-            Console.Write("1. Add an Expense:");
-
             
+            Console.WriteLine("\nMenu: \n");
+            int[] numbers =  { 1, 2, 3, 4};
+            int _range;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("1. Add an Expense: \n");
+                    Console.Write("2. View Expenses and Budget: \n");
+                    Console.Write("3. Remove an Expense: \n");
+                    Console.Write("4. Exit \n");
+                    Console.Write("Enter your choice: ");
+                    _range = int.Parse(Console.ReadLine());
+                    if (_range >= 5) throw new Exception("Number not on menu.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nInvalid input: {ex.Message} \nSelect a number on the menu.");
+                }
+            }
+
+           
+            if ( _range == 1)
+                {
+                Console.Write("Enter the expense name: ");
+                Console.ReadLine();
+                Console.Write("Enter the expense amount: ");
+                Console.ReadLine();
+                Console.WriteLine("Expense added successfully");
+                }
+
+            if (_range == 2)
+            {
+                //enter option 2
+            }
+
+            if ( _range == 3)
+            {
+                //enter option 3
+            }
+
+            if ( _range == 4)
+            {
+                //enter option 4
+            }
+
         }
     }
 }
