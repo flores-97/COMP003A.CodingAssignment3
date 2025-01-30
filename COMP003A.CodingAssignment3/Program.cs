f8cfadcff9bf810a72e694bcb8e5dd9b692d1099
flores-97 <floresvictor15@gmail.com>
2025-01-29 19:50:31 -0800

    began option 2 and 4. need to be able to exit option 1 if selected no

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -15,7 +15,7 @@ namespace COMP003A.CodingAssignment3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Budget Management Tool!\n");
-
+            
             int range;
 
             while (true)
@@ -33,54 +33,144 @@ namespace COMP003A.CodingAssignment3
                     //user's income 
                 }
             }
-            
-            Console.WriteLine("\nMenu: \n");
-            int[] numbers =  { 1, 2, 3, 4};
-            int _range;
-            while (true)
-            {
-                try
-                {
-                    Console.Write("1. Add an Expense: \n");
-                    Console.Write("2. View Expenses and Budget: \n");
-                    Console.Write("3. Remove an Expense: \n");
-                    Console.Write("4. Exit \n");
-                    Console.Write("Enter your choice: ");
-                    _range = int.Parse(Console.ReadLine());
-                    if (_range >= 5) throw new Exception("Number not on menu.");
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"\nInvalid input: {ex.Message} \nSelect a number on the menu.");
-                }
-            }
 
            
-            if ( _range == 1)
+            
+            
+            int[] numbers =  { 1, 2, 3, 4};
+            int _range;
+            
+                Console.WriteLine("\nMenu: \n");
+
+
+                while (true)
                 {
-                Console.Write("Enter the expense name: ");
-                Console.ReadLine();
-                Console.Write("Enter the expense amount: ");
-                Console.ReadLine();
-                Console.WriteLine("Expense added successfully");
+                    try
+                    {
+                        Console.Write("1. Add an Expense: \n");
+                        Console.Write("2. View Expenses and Budget: \n");
+                        Console.Write("3. Remove an Expense: \n");
+                        Console.Write("4. Exit \n");
+                        Console.Write("Enter your choice: ");
+                        _range = int.Parse(Console.ReadLine());
+                        if (_range >= 5) throw new Exception("Number not on menu.");
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"\nInvalid input: {ex.Message} \nSelect a number on the menu.");
+                    }
                 }
 
-            if (_range == 2)
-            {
-                //enter option 2
-            }
+            string expenseName1;
+            string expenseName2;
+            string expenseName3;
+            string expenseName4;
+            string expenseName5;
+            int expenseValue1;
+            int expenseValue2;
+            int expenseValue3;
+            int expenseValue4;
+            int expenseValue5;
 
-            if ( _range == 3)
-            {
-                //enter option 3
-            }
 
-            if ( _range == 4)
-            {
-                //enter option 4
-            }
+            if (_range == 1)
+                while (true)
+                {
+                    Console.Write("Enter the expense name: ");
+                    expenseName1 = Console.ReadLine();
+                    Console.Write("Enter the expense amount: ");
+                    expenseValue1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter another expense? (yes/no)");
+                    string otherExp = Console.ReadLine().ToLower();
+                    // first expense
 
+                    if (otherExp == "yes")
+                    {
+                        Console.Write("Second expense name: ");
+                        expenseName2 = Console.ReadLine();
+                        Console.Write("Second expense value: ");
+                        expenseValue2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter another expense? (yes/no)");
+                        string otherExp2 = Console.ReadLine().ToLower();
+                        //second expense
+                        if (otherExp2 == "yes")
+                        {
+                            Console.Write("Third expense name: ");
+                            expenseName3 = Console.ReadLine();
+                            Console.Write("Third expense value: ");
+                            expenseValue3 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter another expense? (yes/no)");
+                            string otherExp3 = Console.ReadLine().ToLower();
+                            //fourth expense
+                            if (otherExp3 == "yes")
+                            {
+                                Console.Write("Fourth expense name: ");
+                                expenseName4 = Console.ReadLine();
+                                Console.Write("Fourth expense value: ");
+                                expenseValue4 = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter another expense? (yes/no)");
+                                string otherExp4 = Console.ReadLine().ToLower();
+                                //fifth expense
+
+                                    if (otherExp4 == "yes")
+                                    {
+                                        Console.Write("Fifth expense name: ");
+                                        expenseName5 = Console.ReadLine();
+                                        Console.Write("Fifth expense Value");
+                                        expenseValue5 = int.Parse(Console.ReadLine());
+                                        Console.Write("Limit reached. \nGo back to menu");
+                                    }
+
+                                    else if (otherExp4 == "no")
+                                    {
+                                    Console.WriteLine("Expense added successfully!");
+                                    break;
+                                    }
+                                else if (otherExp3 == "no")
+                               {
+                                 Console.WriteLine("Expense added successfully!");
+                                 break;
+                               }
+
+                            }
+                        else if (otherExp2 == "no")
+                           {
+                             Console.WriteLine("Expense added successfully");
+                             break;
+                           }
+                        }
+                    else if (otherExp == "no")
+                      {
+                        Console.WriteLine("Expense added successfully");
+                        break;
+                      }
+                        
+                    }
+
+                    if (_range == 2)
+                    {
+                        //enter option 2
+                        Console.WriteLine($"Expenses: \n");
+                        Console.Write($"-{expenseName1}: ${expenseValue1}\n");
+                        Console.Write($"-");
+
+                    }
+
+                    if (_range == 3)
+                    {
+                        //enter option 3
+                        Console.WriteLine("Enter the expense you want to remove: ");
+                        
+                    }
+
+                    if (_range == 4)
+                    {
+                        //enter option 4
+                        Console.WriteLine("Goodbye!");
+                        System.Environment.Exit(0);
+                    }
+                }
         }
     }
 }
